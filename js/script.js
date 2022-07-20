aeaef335fbea3365f9afdd86366bb7a78add4248
Shrouk Hussein <shrouq.hussein.elsayed@gmail.com>
2022-07-20 23:30:32 +0200

    added script

---
+++ js/script.js
@@ -0,0 +1,195 @@
+// places data image, name, brief
+let places = [
+    {
+        id: "card1",
+        image: "images/camels.jpg",
+        name: "Egypt tangiba",
+        brief: "dentibus et magnis dis parturient montes, nascetur ridiculus mus",
+        date: "augst,2015",
+        num_comments: 22,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+    }
+    ,
+    {
+        id: "card2",
+        image: "images/aswan.jpg",
+        name: "olling phisotip",
+        brief: "penatibus et magins dis parturient montes , nascetur ridiculus mus",
+        date: "augst 5,2013",
+        num_comments: 13,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card3",
+        image: "images/Loreto-Fishing.avif",
+        name: "cortia dyna",
+        brief: "penatibus et magins dis parturient montes , nascetur ridiculus mus",
+        date: "augst 5,2013",
+        num_comments: 8,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card4",
+        image: "images/download.jpg",
+        name: "sysne of dubai",
+        brief: "penatibus et magins dis parturient montes , nascetur ridiculus mus",
+        date: "augst 5,2013",
+        num_comments: 7,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card5",
+        image: "images/gettyimages-1203902772-612x612.jpg",
+        name: "Egypt tangiba",
+        brief: "dentibus et magnis dis parturient montes, nascetur ridiculus mus",
+        date: "augst,2015",
+        num_comments: 22,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card6",
+        image: "images/Loreto-Fishing.avif",
+        name: "olling phisotip",
+        brief: "penatibus et magins dis parturient montes , nascetur ridiculus mus",
+        date: "augst 5,2013",
+        num_comments: 13,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card7",
+        image: "images/aswan.jpg",
+        name: "cortia dyna",
+        brief: "penatibus et magins dis parturient montes , nascetur ridiculus mus",
+        date: "augst 5,2013",
+        num_comments: 8,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card8",
+        image: "images/camels.jpg",
+        name: "sysne of dubai",
+        brief: "penatibus et magins dis parturient montes , nascetur ridiculus mus",
+        date: "augst 5,2013",
+        num_comments: 7,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card9",
+        image: "images/Loreto-Fishing.avif",
+        name: "Egypt tangiba",
+        brief: "dentibus et magnis dis parturient montes, nascetur ridiculus mus",
+        date: "augst,2015",
+        num_comments: 22,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card10",
+        image: "images/aswan.jpg",
+        name: "olling phisotip",
+        brief: "penatibus et magins dis parturient montes , nascetur ridiculus mus",
+        date: "augst 5,2013",
+        num_comments: 13,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card11",
+        image: "images/gettyimages-1203902772-612x612.jpg",
+        name: "cortia dyna",
+        brief: "penatibus et magins dis parturient montes , nascetur ridiculus mus",
+        date: "augst 5,2013",
+        num_comments: 8,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+    {
+        id: "card12",
+        image: "images/download.jpg",
+        name: "sysne of dubai",
+        brief: "penatibus et magins dis parturient montes , nascetur ridiculus mus",
+        date: "augst 5,2013",
+        num_comments: 7,
+        full_desciption:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown pr specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem.",
+
+    }
+    ,
+
+]
+
+$("document").ready(
+    console.log("ddddd"),
+    fillData(),
+    createModals() 
+
+)
+function fillData() {
+    places.forEach(function (card) {
+        $(".cards-container").append(`
+        <div class="col-sm-10 col-md-5 col-lg-3 card" data-bs-toggle="modal" data-bs-target=${"#" + card.id}>
+                <img src=${card.image} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title font-primary-color">${card.name}</h5>
+                    <p class="card-text">${card.brief}
+                    </p>
+                    <div class="card-info row">
+                        <p class="col-6"><i class="far fa-calendar"></i> ${card.date}</p>
+                        <p class="col-6"><i class="fa fa-comments-o"></i><span class="num-comments">${card.num_comments}</span> comments
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `)
+
+    })
+
+}
+function createModals() {
+    places.forEach(function (card) {
+        $("body").append(
+            ` 
+        <div class="modal fade" id=${card.id} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body  my-modal-body container ">
+            <div class="row">
+            <div class="m-2 col-lg-5 col-md-10 col-sm-10 col-xs-10"><img src="${card.image}" alt="${card.name}" class="modal-img"></div>
+            <div class="m-2 col-lg-5 col-md-10 col-sm-10 col-xs-10">
+                <h4 class=" font-primary-color modal-title"> ${card.name}</h4>
+                    <p class="place-description">
+                    ${card.full_desciption}
+
+                    </p>
+                
+        </div>
+            </div>
+     
+              
+            </div>
+          
+          </div>
+        </div>
+      </div>`
+      )
+    })
+}
